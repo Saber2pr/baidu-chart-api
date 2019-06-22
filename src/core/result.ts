6fcfe38069d84e01b8b022bb99805b6539846d40
saber2pr <saber2pr@gmail.com>
2019-06-22 16:50:49 +0000

    feat(result.ts): add result interface

---
+++ src/core/result.ts
@@ -0,0 +1,12 @@
+/*
+ * @Author: saber2pr
+ * @Date: 2019-06-22 15:12:28
+ * @Last Modified by: saber2pr
+ * @Last Modified time: 2019-06-22 15:59:04
+ */
+export interface SearchResult {
+  word: string
+  startDate: string
+  endDate: string
+  data: string
+}
