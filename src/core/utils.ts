f9932872dae6dadf79f7bda2f222e3d01eda8f61
saber2pr <saber2pr@gmail.com>
2019-06-22 10:48:02 +0000

    refactor(utils): use @saber2pr/fp/list/zip

--- src/core/utils.ts
+++ src/core/utils.ts
@@ -2,21 +2,15 @@
  * @Author: saber2pr
  * @Date: 2019-06-21 20:56:35
  * @Last Modified by: saber2pr
- * @Last Modified time: 2019-06-21 21:00:24
+ * @Last Modified time: 2019-06-22 10:00:47
  */
-export const zip = <A extends PropertyKey, B extends PropertyKey>(
-  a: A[],
-  b: B[]
-) =>
-  a.length > b.length
-    ? zip(b, a)
-    : a.reduce((receiver, x, i) => ({ ...receiver, [x]: b[i] }), {})
+import { zip } from '@saber2pr/fp/lib/list'
 
 export const createPtbkMap = (ptbk: string) => {
   const m = ptbk.length / 2
   const key = ptbk.slice(0, m).split('')
   const value = ptbk.slice(m).split('')
-  return zip(key, value)
+  return zip(key)(value)
 }
 
 export const transformDataFromPtbk = (ptbk: string, data: string) => {
